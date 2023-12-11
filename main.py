b16048f45695da850af5b30dd0c2eec971945479
Shannon Ruan <90012158+ShannoonRuaaa@users.noreply.github.com>
2023-12-10 20:17:26 -0800

    Main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import random as rd
+
+
+def BOGO(arr1):
+    # Use a breakpoint in the code line below to debug your script.
+    arr2 = []
+    for i in range(0, len(arr1)):
+        arr2.append(arr1.pop(rd.randint(0, len(arr1) - 1)))
+    return arr2
+
+
+# Press the green button in the gutter to run the script.
+list = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+def BOGOsort(list):
+    sorted = False
+    while (not sorted):
+        list = BOGO(list)
+        for i in range(0, len(list) - 1):
+            if (list[i] > list[i + 1]):
+                sorted = False
+                break
+            else:
+                sorted = True
+    return list
+
+print(BOGOsort(list))
